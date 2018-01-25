67dc49979f6da18881087413f3d3793eae3ac109
rohangho <rohangho@live.com>
2018-01-25 21:45:36 +0530

    Added screenshot

---
+++ Screenshot1/app/src/main/java/com/getterexample/android/screenshot1/CustomClass.java
@@ -0,0 +1,37 @@
+package com.getterexample.android.screenshot1;
+
+/**
+ * Created by ROHAN on 25-01-2018.
+ */
+
+public class CustomClass {
+
+    int image;
+    String name;
+    CustomClass()
+    {
+
+    }
+
+    public CustomClass(String s, int resourceId) {
+        this.image=resourceId;
+        this.name=s;
+    }
+
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
