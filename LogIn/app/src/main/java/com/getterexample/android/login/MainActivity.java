242fe2b6e26c1ccc0dacabfa7000072f6907b06c
rohangho <rohangho@live.com>
2018-01-26 15:21:29 +0530

    Added five layout

--- LogIn/app/src/main/java/com/getterexample/android/login/MainActivity.java
+++ LogIn/app/src/main/java/com/getterexample/android/login/MainActivity.java
@@ -1,71 +1,44 @@
-package com.getterexample.android.screenshot1;
+package com.getterexample.android.login;
 
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<CustomClass> products=new ArrayList<CustomClass>();
-    ListAdapter adapt;
-    String[] name;
-    TypedArray im;
-    ListView l;
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    return true;
-                case R.id.offer:
-                    return true;
-                case R.id.basket:
+                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
-
             return false;
         }
     };
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mTextMessage = (TextView) findViewById(R.id.message);
-        l=(ListView)findViewById(R.id.list);
-        name=getResources().getStringArray(R.array.productlist);
-        im=getResources().obtainTypedArray(R.array.picture);
        // BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
        // navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        for(int i=0;i<name.length;i++)
-        {
-            CustomClass cc=new CustomClass(name[i],im.getResourceId(i,-1));
-            products.add(cc);
-        }
-        ListAdapter adapter=new ListAdapter(this,products);
-        l.setAdapter(adapter);
     }
-
-
 
 }
